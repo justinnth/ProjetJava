adaf42676661905f32a567322d923835c28b6152
Bernier Corentin <corentin.bernier97@gmail.com>
2017-12-23 16:48:19 +0100

    Changement de nom pour le projet et ajout du diagramme de Class fait sous StarUML

--- JeuCartes/src/JeuCartes/JeuCartes.java
+++ JeuCartes/src/JeuCartes/JeuCartes.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package morpion;
+package JeuCartes;
 
 /**
  *
  * @author justinnorth
  */
-public class Morpion {
+public class JeuCartes {
 
     /**
      * @param args the command line arguments
